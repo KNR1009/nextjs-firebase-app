53be84441150d6d876dec5bd13a1d2b52ea851de
伊東和成 <itokazunari5375@icloud.com>
2021-06-27 08:18:28 +0900

    認証処理の追加

---
+++ lib/authentication.ts
@@ -0,0 +1,32 @@
+import firebase from "firebase/app";
+
+function authenticate() {
+  // ユーザー認証が通っているかのチェック
+  firebase
+    .auth()
+    .signInAnonymously()
+    .catch(function (error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // ...
+    });
+
+
+  firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+      console.log(user.toJSON());
+      console.log(user.uid);
+      console.log(user.isAnonymous);
+    } else {
+      // User is signed out.
+      // ...
+      console.log("ログインしていません")
+    }
+    // ...
+  });
+}
+
+if (process.browser) {
+  authenticate();
+}
